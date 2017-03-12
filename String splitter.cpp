1f99c0d454780be66d63e64df44b5e7e25e842a4
Tejas Dastane <tejas.dastane@gmail.com>
2017-03-12 17:35:32 +0530

    Add files via upload

---
+++ String splitter.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include<string>
+#include<stdio.h>
+using namespace std;
+
+class split_s{
+    private:
+        string s,delim;
+        int i;
+        bool more,sp_ret,delim_return;
+    public:
+        split_s(string str,string lim,bool lim_return,bool spaces_return){
+            s = str;
+            delim = lim;
+            delim_return = lim_return;
+            sp_ret = spaces_return;
+
+            i=0;
+            more = true;
+        }
+
+        split_s(string str,string lim,bool lim_return){
+            s = str;
+            delim = lim;
+            delim_return = lim_return;
+            sp_ret = true;
+
+            i=0;
+            more = true;
+        }
+
+        split_s(string str,string lim){
+            s = str;
+            delim = lim;
+            delim_return = true;
+            sp_ret = true;
+
+            i=0;
+            more = true;
+        }
+
+        split_s(string str){
+            s = str;
+            delim = "";
+            delim_return = true;
+            sp_ret = true;
+
+            i=0;
+            more = true;
+        }
+
+        split_s(string str, bool spaces_return){
+            s = str;
+            delim = "";
+            delim_return = true;
+            sp_ret = spaces_return;
+
+            i=0;
+            more = true;
+        }
+
+        string next(){
+            string nextstr="";
+            bool start = false;
+            for(;i<s.size();i++){
+                if(delim.find(s[i])!=string::npos){
+                    if(delim_return == true){
+                        if(nextstr == ""){
+                            if((s.at(i) == ' ' || s.at(i) == '\t') && sp_ret==false) continue;
+                            nextstr += s[i];
+                        }
+                        else i--;
+                        i++;
+                        return nextstr;
+                    }
+                    else{
+                        start=true;
+                        continue;
+                    }
+                }
+                else if(start==true && delim.find(s[i])==string::npos){
+                    start=false;
+                    return nextstr;
+                }
+                if((s.at(i) == ' ' || s.at(i) == '\t') && sp_ret==false) continue;
+                nextstr += s[i];
+            }
+            more = false;
+            return nextstr;
+        }
+
+        bool has_more(){
+            if(delim_return == false)
+                for(;i<s.size();i++){
+                    if(delim.find(s[i]) == string::npos){
+                        if((s.at(i) == ' ' || s.at(i) == '\t') && sp_ret==false) continue;
+                        more = true;
+                        break;
+                    }
+                    if(i==s.size()-1) more = false;
+                }
+            return more;
+        }
+};
+
+//int main(){
+//    string str;
+//    getline(cin,str);
+//
+//    split_s sp(str,"+-",false,false);
+//    while(sp.has_more()){
+//        string x = sp.next();
+//        cout<<x<<endl;
+//    }
+//    cout<<"Program terminates";
+//}
